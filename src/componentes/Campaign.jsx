88d2f49c2660183e2acc870741ed776170e88ac6
yykyeong <yykyeong@naver.com>
2023-08-10 16:59:06 +0900

    campaign

--- src/componentes/Campaign.jsx
+++ src/componentes/Campaign.jsx
@@ -3,9 +3,77 @@
 function Campaign () {
 
   return (
-    <div>
-    Campaign
-    </div>
+    <section id="campaign">
+      <div className="campaign-area">
+        <h2 className="campaign-title">상하목장 캠페인 <em>Organic Circle</em></h2>
+        <div className="campaign-list">
+          <article className="campaign-item c1">
+            <span className="num">1.</span>
+            <h2 className="title">
+              <p>순환하는 자연 그대로의 방식</p>
+              <p>유기농</p>
+            </h2>
+            <div className="desc">
+              <div>
+                식품도 효율성이 우선시 되는 사회
+                생산성은 놀라우리 만치 증대되었지만 자연이 지닌 순환과 균형은
+                파괴될 수 밖에 없었습니다.
+              </div>
+              <div>
+                그 결과는 예측하지 못했던 환경 오염, 기후 변화, 가축 질병으로
+                오늘날 우리에게 돌아오고 있습니다.
+              </div>
+              <div>
+                상하목장 유기농은 생산성을 위한 화학비료, GMO작물, 성장호르몬,
+                좁고 폐쇄된 축사 대신, 자연의 순환을 회복시켜, 자연 그대로의
+                모습으로 소들을 키우고자 하는 노력의 결과 입니다.
+              </div>
+            </div>
+          </article>
+          <article className="campaign-item c2">
+            <span className="num">2.</span>
+            <h2 className="title">
+              <p>자연을 순환시키는 놀라운 연결고리</p>
+              <p>유기농 퇴비</p>
+            </h2>
+            <div className="desc">
+              <div>
+                상하목장의 순환에는 젖소와 초지, 그리고 그 가운데 귀한 퇴비가 있습니다.
+                상하목장 목장주들은 유기농 젖소의 퇴비로 유기농 초지를 가꾸고,
+                유기농 초지에서 난 풀들은 유기농 젖소에게 다시 먹입니다.
+                땅과 물을 오염시키는 줄만 알았던 소의 분뇨가 소중한 유기농 퇴비가 되어
+                자연을 순환시키는 놀라운 연결고리가 되어 주는 것입니다.
+              </div>
+              <div>
+                땅에서 난 것은 동물에게, 동물에게 난 것은 다시 땅으로 돌라가는
+                건강한 순환의 고리, 바로 상하목장의 오가닉 서클을 만들어 주는 것이지요.
+                우리가 유기농을 시작하기 전에는 차마 알지 못했던 순환하는
+                자연의 놀라운 힘을 상하목장은 배워가고 있습니다.
+              </div>
+            </div>
+          </article>
+          <article className="campaign-item c3">
+            <span className="num">3.</span>
+            <h2 className="title">
+              <p>건강한 순환의 고리,</p>
+              <p>오가닉 서클로의 초대</p>
+            </h2>
+            <div className="desc">
+              <div>
+                상하목장은 여러분에게 ‘자연의 순환’을 선물해 드리고자 합니다.
+                집 안 공간에 상하목장 유기농 퇴비로 작은 자연을 가꿔보는 것이지요.
+                자연을 소비하는 존재에서 벗어나, 직접 자연을 싹 틔우고, 열매를 맺어 보는 것은,
+                순환하는 자연의 경이로움에 대해 생각하고 경험해 볼 수 있는 좋은 기회가 될 것 입니다.
+              </div>
+              <div>
+                건강한 순환의 고리, 상하목장 오가닉 서클(Organic Circle)캠페인에 여러분을 초대합니다.
+                여러분의 베란다에서부터 오가닉 서클의 녹색 고리를 시작해 보세요
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
   )
 }
 
